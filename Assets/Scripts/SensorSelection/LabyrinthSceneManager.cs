8efedf79c128ecaca5d41c753d9cd9ef5d47b365
alu0101049151 <alu0101049151@ull.edu.es>
2021-07-31 18:06:41 +0100

    Developing maximizeButton action

--- Assets/Scripts/SensorSelection/LabyrinthSceneManager.cs
+++ Assets/Scripts/SensorSelection/LabyrinthSceneManager.cs
@@ -26,8 +26,18 @@ public class LabyrinthSceneManager : MonoBehaviour
     private GameObject challengeViewer;
     private GameObject mainCanvas;
 
-    public Camera robotCamera;
-
+    // Variables used for the rescale of the challengeViewer.
+    private RectTransform canvasRect;
+    private RectTransform challengeViewerRect;
+    private float canvasWidth;
+    private float canvasHeight;
+    private float firstChallengeWidth;
+    private float firstChallengeHeight;
+    private Vector3 originalChallengeViewerPos;
+    private Vector3 mainCanvasPosition;
+    private GameObject maximizeButton;
+    private RectTransform maximizeButtonRect;
+    private Vector3 maximizeButtonInitPos;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -35,16 +45,11 @@ public class LabyrinthSceneManager : MonoBehaviour
     /// </summary>
     void Start()
     {
-        robotCamera = robotCamera.GetComponent<Camera>();
         SetUpSelectedRobot();
         challengeViewer = GameObject.Find("Canvas/ChallengeViewer");
+        maximizeButton = GameObject.Find("Canvas/MaximizeButton");
         mainCanvas = GameObject.Find("Canvas");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        changeImageScaleSetUp();
     }
 
     /// <summary>
@@ -72,23 +77,41 @@ public class LabyrinthSceneManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    public void changeImageScaleSetUp()
+    {
+        canvasRect = mainCanvas.GetComponent<RectTransform>();
+        challengeViewerRect = challengeViewer.GetComponent<RectTransform>();
+        maximizeButtonRect = maximizeButton.GetComponent<RectTransform>();
+        canvasWidth = canvasRect.rect.width;
+        canvasHeight = canvasRect.rect.height;
+        firstChallengeWidth = challengeViewerRect.rect.width;
+        firstChallengeHeight = challengeViewerRect.rect.height;
+        originalChallengeViewerPos = challengeViewer.transform.position;
+        mainCanvasPosition = mainCanvas.transform.position;
+        maximizeButtonInitPos = maximizeButton.transform.position;
+    }
+
     public void changeImageScale()
     {
-        RectTransform canvasRect = mainCanvas.GetComponent<RectTransform>();
-        RectTransform challengeViewerRect = challengeViewer.GetComponent<RectTransform>();
-        RectTransform cameraRect = robotCamera.GetComponent<RectTransform>();
-        float canvasWidth = canvasRect.rect.width;
-        float canvasHeight = canvasRect.rect.height;
         float challengeWidth = challengeViewerRect.rect.width;
         float challengeHeight = challengeViewerRect.rect.height;
         if((challengeWidth != canvasWidth) && (challengeHeight != canvasHeight))
         {
             challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, canvasHeight);
             challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, canvasWidth);
+            challengeViewer.transform.position = mainCanvasPosition;
+            float buttonOffset = (maximizeButtonRect.rect.width / 2);
+            float auxWidth = (-canvasWidth/2);
+            float auxHeight = (canvasHeight/2);
+            Debug.Log ("auxWidth: " + auxWidth + " auxHeigth: " + auxHeight);
+            float z = canvasRect.position.z;
+            maximizeButtonRect.localPosition = new Vector3(auxWidth + buttonOffset, auxHeight - buttonOffset, canvasRect.position.z);
+            //maximizeButton.transform.position = new Vector3((-canvasWidth/2), (canvasHeight/2), originalChallengeViewerPos.z);
         } else {
-            challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, DEFAULT_HEIGHT);
-            challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, DEFALUT_WIDTH);
+            challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, firstChallengeHeight);
+            challengeViewerRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, firstChallengeWidth);
+            challengeViewer.transform.position = originalChallengeViewerPos;
+            maximizeButton.transform.position = maximizeButtonInitPos;
         }
     }
-
 }
