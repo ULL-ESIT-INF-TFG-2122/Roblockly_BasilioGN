547caf1e4fae25ed526d18ac78aac6a6412a06df
alu0101049151 <alu0101049151@ull.edu.es>
2021-08-26 01:33:08 +0100

    Developing StatisticsManager behaviour

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public GameObject statisticsPanel;
     public GameObject statisticsInfoPanel;
-    // Start is called before the first frame update
+    private GameObject statisticsManager;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
     void Start()
     {
-        
+        statisticsManager = GameObject.FindWithTag("StatisticsManager");
     }
 
     public void StatisticsPanelActivation()
@@ -19,6 +25,7 @@ public class SettingsMenu : MonoBehaviour
         {
             statisticsPanel.SetActive(false);
         } else {
+            ActivateCompletedChallengesButtons();
             statisticsPanel.SetActive(true);
         }
     }
@@ -31,6 +38,62 @@ public class SettingsMenu : MonoBehaviour
         } else {
             statisticsInfoPanel.SetActive(true);
         }
+    }
+
+    /// <summary>
+    /// Activates the number of buttons required based on the number of 
+    /// completed challenges found in the dictionary.
+    /// </summary>
+    public void ActivateCompletedChallengesButtons()
+    {
+        Dictionary<string, List<ChallengeSolution>> auxSolutions = statisticsManager.GetComponent<StatisticsManager>().GetChallengesSolutions();
+        if (auxSolutions != null)
+        {
+            foreach (var challengeSolution in auxSolutions)
+            {
+                int i = 1;
+                bool found = false;
+                while(!found && (i < 5))
+                {
+                    if (!statisticsPanel.transform.GetChild(i).gameObject.activeSelf)
+                    {
+                        found = true;
+                        statisticsPanel.transform.GetChild(i).gameObject.SetActive(true);
+                        statisticsPanel.transform.GetChild(i).transform.GetChild(0).GetComponent<Text>().text = GetChallengeButtonText(challengeSolution.Key);
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Given a key of an element from ChallengesSolutions dictionary, returns 
+    // the text to be displayed on the buttons of "StatisticsPanel" GUI.
+    /// </summary>
+    /// <param name="challengeName"> The string with the key to be "translated" </param>
+    private string GetChallengeButtonText(string challengeName)
+    {
+        string challengeButtonText;
+        switch (challengeName)
+        {
+            case "Labyrinth": 
+                challengeButtonText = "Reto del laberinto";
+                break;
+            case "IR":
+                challengeButtonText = "Reto del sendero blanco";
+                break;
+            case "Color":
+                challengeButtonText = "Reto del sendero de colores";
+                break;
+            case "Gyroscope": 
+                challengeButtonText = "Reto de la plataforma móvil";
+                break;
+            default:
+                challengeButtonText = "Reto de roblockly";
+                break;
+        }
+        return challengeButtonText;
     }
 
     public void CancelMenu()
--- Assets/Scripts/StatisticsManager.cs
+++ Assets/Scripts/StatisticsManager.cs
@@ -6,13 +6,15 @@ public class StatisticsManager : MonoBehaviour
 {
     public static StatisticsManager statisticsManagerInstance;
 
+    // This dictionary stores all the challenges that have been completed and their respective solutions.
     private Dictionary<string, List<ChallengeSolution>> challengesSolutions;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake()
-    {
+    {   // Adding singleton pattern to keep the statistics manger through all   
+        // the scenes
         if (StatisticsManager.statisticsManagerInstance == null)
         {
             StatisticsManager.statisticsManagerInstance = this;
@@ -21,6 +23,28 @@ public class StatisticsManager : MonoBehaviour
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Adds a new solution for a specific challenge if it exists as a key of
+    /// "challengesSolutions" or adds it as new element to the dictionary.
+    /// </summary>
+    /// <param name="challengeName"> Is the challenge name used as key to look for in the dictionary. </param>
+    /// <param name="solution"> Is the new solution to add to a specific challenge (key). </param>
+    public void AddNewChallengeSolution(string challengeName, ChallengeSolution solution)
+    {
+        if (challengesSolutions.ContainsKey(challengeName))
+        {
+            challengesSolutions[challengeName].Add(solution);
+        } else {
+            List<ChallengeSolution> newListToAdd = new List<ChallengeSolution>();
+            newListToAdd.Add(solution);
+            challengesSolutions.Add(challengeName, newListToAdd);
+        }
+    }
+
+    public Dictionary<string, List<ChallengeSolution>> GetChallengesSolutions()
+    {
+        return challengesSolutions;
+    }
     
 
         
